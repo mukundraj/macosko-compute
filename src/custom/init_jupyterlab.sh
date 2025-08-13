963c612cc1e90b013f8d57153b378f4b81b15fb0
Mukund Raj <1138446+mukundraj@users.noreply.github.com>
2025-08-13 10:12:26 -0400

    add init_jupyterlab.sh

---
+++ src/custom/init_jupyterlab.sh
@@ -0,0 +1,22 @@
+init_jupyterlab() {
+    # Check if the 'jupyterlab' environment exists
+    if micromamba env list | grep -q "jupyterlab"; then
+        echo "JupyterLab environment found. Activating and starting JupyterLab..."
+        micromamba activate jupyterlab
+        jupyter lab --allow-root --ip '0.0.0.0' --port '8787' --NotebookApp.token='' --NotebookApp.notebook_dir='/workdir' --no-browser  &
+    else
+        echo "JupyterLab environment not found. Creating and starting JupyterLab..."
+        # Create the environment
+        micromamba create -n jupyterlab -y python=3.11.2 jupyterlab pandas -c conda-forge
+
+        # Install the R kernel for JupyterLab
+        micromamba run -n jupyterlab R -e 'IRkernel::installspec(user = FALSE)'
+
+        # Activate the new environment
+        micromamba activate jupyterlab
+
+
+        # Start JupyterLab
+        jupyter lab --allow-root --ip '0.0.0.0' --port '8787' --NotebookApp.token='' --NotebookApp.notebook_dir='/workdir' --no-browser &
+    fi
+}
