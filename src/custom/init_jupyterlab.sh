fe093b5a0a034268eba8709c7db62a73c5d9ddfd
Mukund Raj <1138446+mukundraj@users.noreply.github.com>
2025-08-13 11:07:30 -0400

    set custom jupyter pass

--- src/custom/init_jupyterlab.sh
+++ src/custom/init_jupyterlab.sh
@@ -8,10 +8,22 @@ init_jupyterlab() {
         echo "JupyterLab environment not found. Creating and starting JupyterLab..."
         # Create the environment
 
-       # set password 
-       expect -c 'spawn bash -lc "micromamba run jupyter lab password"; expect "Enter password:"; send "$env(USER)\r"; expect "Verify password:"; send "$env(USER)\r"; expect eof'
+
 
        micromamba create -n jupyterlab -y python=3.11.2 jupyterlab pandas -c conda-forge
+
+       # set password 
+       expect -c '
+       spawn bash -lc "micromamba run -n jupyterlab jupyter lab password"
+       expect "Enter password:"
+       send "$env(USER)\r"
+       expect "Verify password:"
+       send "$env(USER)\r"
+       expect eof
+       '
+       # expect -c 'spawn bash -lc "micromamba run -n jupyterlab jupyter lab password"; expect "Enter password:"; send "$USER\r"; expect "Verify password:"; send "$USER\r"; expect eof'
+
+       micromamba run -n jupyterlab jupyter lab --generate-config && echo -e 'c.ServerApp.terminals_enabled = True\nc.FileContentsManager.delete_to_trash = False' >> /root/.jupyter/jupyter_lab_config.py
 
         # Install the R kernel for JupyterLab
         micromamba run -n jupyterlab R -e 'IRkernel::installspec(user = FALSE)'
