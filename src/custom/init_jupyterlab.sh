3297cabefbf927e1b09105dc3b5577a7c7734063
Mukund Raj <1138446+mukundraj@users.noreply.github.com>
2025-08-13 14:27:24 -0400

    create common_setup fn

--- src/custom/init_jupyterlab.sh
+++ src/custom/init_jupyterlab.sh
@@ -1,20 +1,5 @@
-init_jupyterlab() {
-    # Check if the 'jupyterlab' environment exists
-    if micromamba env list | grep -q "jupyterlab"; then
-        echo "JupyterLab environment found. Activating and starting JupyterLab..."
-        micromamba activate jupyterlab
-        jupyter lab --allow-root --ip '0.0.0.0' --port '8787' --NotebookApp.token='' --NotebookApp.notebook_dir='/workdir' --no-browser
-    else
-        echo "JupyterLab environment not found. Creating and starting JupyterLab..."
-        # Create the environment
 
-
-
-       micromamba create -n jupyterlab -y python=3.11.2 jupyterlab pandas -c conda-forge
-
-       # Activate the new environment
-       micromamba activate jupyterlab
-
+common_setup(){
        # set password 
        expect -c '
        spawn bash -lc "micromamba run -n jupyterlab jupyter lab password"
@@ -32,6 +17,27 @@ init_jupyterlab() {
         micromamba run -n jupyterlab R -e 'IRkernel::installspec(user = FALSE)'
 
 
+}
+
+init_jupyterlab() {
+    # Check if the 'jupyterlab' environment exists
+    if micromamba env list | grep -q "jupyterlab"; then
+        echo "JupyterLab environment found. Activating and starting JupyterLab..."
+        micromamba activate jupyterlab
+
+        common_setup
+
+        jupyter lab --allow-root --ip '0.0.0.0' --port '8787' --NotebookApp.token='' --NotebookApp.notebook_dir='/workdir' --no-browser
+    else
+        echo "JupyterLab environment not found. Creating and starting JupyterLab..."
+        # Create the environment
+
+       micromamba create -n jupyterlab -y python=3.11.2 jupyterlab pandas -c conda-forge
+
+       # Activate the new environment
+       micromamba activate jupyterlab
+
+       common_setup
 
         # Start JupyterLab
         jupyter lab --allow-root --ip '0.0.0.0' --port '8787' --NotebookApp.token='' --NotebookApp.notebook_dir='/workdir' --no-browser
