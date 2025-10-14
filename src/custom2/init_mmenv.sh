64779ad4efdc20f483439891ba4407c92403c329
Mukund Raj <1138446+mukundraj@users.noreply.github.com>
2025-10-14 13:44:51 -0400

    revert common setup

--- src/custom2/init_mmenv.sh
+++ src/custom2/init_mmenv.sh
@@ -16,40 +16,9 @@ common_setup(){
 
         # setup IRkernel if renv/activate.R or renv.lock exists, install and setup if not
         echo "workdir: $workdir"
-        micromamba run -n "$envname" R -e "
-            setwd('$workdir')
 
-            # Clear .Rprofile to avoid duplicate entries
-            if (file.exists('.Rprofile')) {
-                file.remove('.Rprofile')
-            }
-
-            if (file.exists('renv/activate.R')) {
-                # Regular restart: load existing renv environment
-                
-                # append micromamba envs path here directly without requiring .Rprofile
-                .libPaths(c(\"/jupyter/micromamba/envs/$envname/lib/R/library\", .libPaths()))
-                renv::load()
-                cat('.libPaths(c(\"/jupyter/micromamba/envs/$envname/lib/R/library\", .libPaths()))',
-                    file = '.Rprofile', append = TRUE, sep = '\n')
-                IRkernel::installspec(user = FALSE)
-            } else if (file.exists('renv.lock')) {
-                # First time start via renv.lock: init and restore
-                renv::init(bare=TRUE)
-                cat('.libPaths(c(\"/jupyter/micromamba/envs/$envname/lib/R/library\", .libPaths()))',
-                    file = '.Rprofile', append = TRUE, sep = '\n')
-                renv::restore()
-                renv::install(c('pbdZMQ', 'IRkernel', 'yaml'))
-                IRkernel::installspec(user = FALSE)
-            } else {
-                # Fresh start: create new renv environment
-                renv::init()
-                cat('.libPaths(c(\"/jupyter/micromamba/envs/$envname/lib/R/library\", .libPaths()))',
-                    file = '.Rprofile', append = TRUE, sep = '\n')
-                renv::install(c('pbdZMQ', 'IRkernel', 'yaml'))
-                IRkernel::installspec(user = FALSE)
-            }
-        "
+        micromamba run -n "$envname" R -e "setwd('$workdir'); if (file.exists('renv/activate.R')) { renv::load(); IRkernel::installspec(user = FALSE) } else if (file.exists('renv.lock')) { renv::init(bare=TRUE); cat('.libPaths(c(\"/jupyter/micromamba/envs/$envname/lib/R/library\", .libPaths()))', file = '.Rprofile', append = TRUE, sep = '\n'); renv::restore(); renv::install(c('pbdZMQ', 'IRkernel', 'yaml')); IRkernel::installspec(user = FALSE) } else { renv::init(); cat('.libPaths(c(\"/jupyter/micromamba/envs/$envname/lib/R/library\", .libPaths()))', file = '.Rprofile', append = TRUE, sep = '\n'); renv::install(c('pbdZMQ','IRkernel', 'yaml')); IRkernel::installspec(user = FALSE) }"
+   
         # first condition - regular restart (only load, don't init renv)
         # second condition - first time start via renv.lock (init_mmenv wont init since micromamba env already exists); (renv.lock must include IRkernel)
         # third condition - first time restart after clearing workdir (init_mmenv wont init renv since micromamba env already exists)
